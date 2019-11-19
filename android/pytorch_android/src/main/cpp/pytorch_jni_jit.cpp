50da5376c96bdaebc31c57b8b0a170c9f98ac3cb
IvanKobzarev <ivankobzarev@fb.com>
2019-11-19 13:39:09 -0800

    [android] Module load with ReadAdapterInterface
    ghstack-source-id: c29e8fccebd6076c6c696292c7cc511c26807114
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30109

--- android/pytorch_android/src/main/cpp/pytorch_jni_jit.cpp
+++ android/pytorch_android/src/main/cpp/pytorch_jni_jit.cpp
@@ -8,10 +8,58 @@
 
 #include <torch/csrc/autograd/record_function.h>
 #include <torch/script.h>
+#include "caffe2/serialize/read_adapter_interface.h"
 
 #include "pytorch_jni_common.h"
 
 namespace pytorch_jni {
+
+class JReadAdapter : public facebook::jni::JavaClass<JReadAdapter> {
+ public:
+  static constexpr auto kJavaDescriptor = "Lorg/pytorch/ReadAdapter;";
+
+  jlong size() {
+    static const auto method =
+        JReadAdapter::javaClassStatic()->getMethod<jlong()>("size");
+    jlong result = method(self());
+    return result;
+  }
+};
+
+class ReadAdapter final : public caffe2::serialize::ReadAdapterInterface {
+ public:
+  explicit ReadAdapter(
+      facebook::jni::alias_ref<JReadAdapter::javaobject> jReadAdapter)
+      : jReadAdapter_(facebook::jni::make_global(jReadAdapter)),
+        size_(jReadAdapter_->size()){};
+
+  size_t size() const override {
+    return size_;
+  }
+
+  size_t read(uint64_t pos, void* buf, size_t n, const char* what = "")
+      const override {
+    if (pos >= size_) {
+      return 0;
+    }
+    facebook::jni::local_ref<facebook::jni::JByteBuffer> jBuf =
+        facebook::jni::JByteBuffer::wrapBytes(static_cast<uint8_t*>(buf), n);
+    static const auto method =
+        JReadAdapter::javaClassStatic()
+            ->getMethod<jint(
+                jlong,
+                facebook::jni::alias_ref<facebook::jni::JByteBuffer>,
+                jint)>("read");
+    jint result = method(jReadAdapter_, pos, jBuf, n);
+    return result > 0 ? result : 0;
+  }
+
+  ~ReadAdapter() {}
+
+ private:
+  facebook::jni::global_ref<JReadAdapter::javaobject> jReadAdapter_;
+  size_t size_ = {};
+};
 
 class PytorchJni : public facebook::jni::HybridClass<PytorchJni> {
  private:
@@ -21,10 +69,16 @@ class PytorchJni : public facebook::jni::HybridClass<PytorchJni> {
  public:
   constexpr static auto kJavaDescriptor = "Lorg/pytorch/NativePeer;";
 
-  static facebook::jni::local_ref<jhybriddata> initHybrid(
+  static facebook::jni::local_ref<jhybriddata> initHybridFilePath(
       facebook::jni::alias_ref<jclass>,
       facebook::jni::alias_ref<jstring> modelPath) {
     return makeCxxInstance(modelPath);
+  }
+
+  static facebook::jni::local_ref<jhybriddata> initHybridReadAdapter(
+      facebook::jni::alias_ref<jclass>,
+      facebook::jni::alias_ref<JReadAdapter::javaobject> jReadAdapter) {
+    return makeCxxInstance(jReadAdapter);
   }
 
 #ifdef TRACE_ENABLED
@@ -38,7 +92,7 @@ class PytorchJni : public facebook::jni::HybridClass<PytorchJni> {
   }
 #endif
 
-  PytorchJni(facebook::jni::alias_ref<jstring> modelPath) {
+  void preModuleLoadSetup() {
     auto qengines = at::globalContext().supportedQEngines();
     if (std::find(qengines.begin(), qengines.end(), at::QEngine::QNNPACK) !=
         qengines.end()) {
@@ -51,13 +105,25 @@ class PytorchJni : public facebook::jni::HybridClass<PytorchJni> {
         /* need_inputs */ false,
         /* sampled */ false);
 #endif
+  }
+
+  PytorchJni(facebook::jni::alias_ref<jstring> modelPath) {
+    preModuleLoadSetup();
     module_ = torch::jit::load(std::move(modelPath->toStdString()));
+    module_.eval();
+  }
+
+  PytorchJni(facebook::jni::alias_ref<JReadAdapter::javaobject> jReadAdapter) {
+    preModuleLoadSetup();
+    module_ = torch::jit::load(torch::make_unique<ReadAdapter>(jReadAdapter));
     module_.eval();
   }
 
   static void registerNatives() {
     registerHybrid({
-        makeNativeMethod("initHybrid", PytorchJni::initHybrid),
+        makeNativeMethod("initHybridFilePath", PytorchJni::initHybridFilePath),
+        makeNativeMethod(
+            "initHybridReadAdapter", PytorchJni::initHybridReadAdapter),
         makeNativeMethod("forward", PytorchJni::forward),
         makeNativeMethod("runMethod", PytorchJni::runMethod),
     });
