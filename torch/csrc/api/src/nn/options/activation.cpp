9e770aa6844f965f787f45a8f07d69cdb0314bf7
Pavel Belevich <pbelevich@fb.com>
2019-11-26 11:14:03 -0800

    C++ API parity: MultiheadAttention
    ghstack-source-id: 4eef95a572dc4148975b09117c144e6908850db0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27309

--- torch/csrc/api/src/nn/options/activation.cpp
+++ torch/csrc/api/src/nn/options/activation.cpp
@@ -31,5 +31,17 @@ LogSoftmaxFuncOptions::LogSoftmaxFuncOptions(int64_t dim) : dim_(dim) {}
 
 } // namespace functional
 
+MultiheadAttentionForwardOptions::MultiheadAttentionForwardOptions(
+    int64_t embed_dim_to_check, int64_t num_heads,
+    Tensor in_proj_weight, Tensor in_proj_bias,
+    Tensor bias_k, Tensor bias_v,
+    bool add_zero_attn, double dropout_p,
+    Tensor out_proj_weight, Tensor out_proj_bias
+  ) : embed_dim_to_check_(embed_dim_to_check), num_heads_(num_heads),
+      in_proj_weight_(in_proj_weight), in_proj_bias_(in_proj_bias),
+      bias_k_(bias_k), bias_v_(bias_v),
+      add_zero_attn_(add_zero_attn), dropout_p_(dropout_p),
+      out_proj_weight_(out_proj_weight), out_proj_bias_(out_proj_bias) {}
+
 } // namespace nn
 } // namespace torch
