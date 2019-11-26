b7a10d6c3dbe12cb2f3b0259d41d0482c9e986e0
Sebastian Messmer <messmer@fb.com>
2019-11-26 14:50:16 -0800

    Boxed variable dispatch
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29934
    
    Previously, when doing boxed dispatch (e.g. custom ops), the dispatcher manually removed the VariableTensorId flag before dispatching
    because custom ops don't have variable kernels.
    This is one of the blockers that prevented us from using the boxed dispatch mechanism for ops from native_functions.yaml because they define variable kernels and need them to be called for autograd.
    
    This PR changes that. The dispatcher doesn't remove the VariableTensorId flag anymore.
    Instead, to make custom ops work, we implement a variable fallback kernel that is called whenever no other variable kernel was found.
    ghstack-source-id: 94618474
    
    Differential Revision: [D18542342](https://our.internmc.facebook.com/intern/diff/D18542342/)

--- aten/src/ATen/function_wrapper.py
+++ aten/src/ATen/function_wrapper.py
@@ -113,10 +113,10 @@ ${return_type} ${api_name}(${type_method_formals}) {
 """)
 
 DEFAULT_UNBOXEDONLY_FUNCTION_REGISTRATION = CodeTemplate("""\
-.op(torch::RegisterOperators::options()
-  .schema("${schema_string}")
-  .impl_unboxedOnlyCatchAllKernel<${return_type} (${formals_types}), &TypeDefault::${api_name}>()
-  .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
+  .op(torch::RegisterOperators::options()
+    .schema("${schema_string}")
+    .impl_unboxedOnlyCatchAllKernel<${return_type} (${formals_types}), &TypeDefault::${api_name}>()
+    .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
 """)
 BACKEND_UNBOXEDONLY_FUNCTION_REGISTRATION = CodeTemplate("""\
 .op(torch::RegisterOperators::options()
@@ -125,9 +125,9 @@ BACKEND_UNBOXEDONLY_FUNCTION_REGISTRATION = CodeTemplate("""\
   .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
 """)
 DEFAULT_FUNCTION_REGISTRATION = CodeTemplate("""\
-.op(torch::RegisterOperators::options()
-  .schema("${schema_string}")
-  .catchAllKernel<${return_type} (${formals_types})>(&TypeDefault::${api_name})
+  .op(torch::RegisterOperators::options()
+    .schema("${schema_string}")
+    .catchAllKernel<${return_type} (${formals_types})>(&TypeDefault::${api_name})
   .aliasAnalysis(c10::AliasAnalysisKind::FROM_SCHEMA))
 """)
 BACKEND_FUNCTION_REGISTRATION = CodeTemplate("""\
